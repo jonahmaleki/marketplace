ffbd421b1110ea26ce9b724b7500bb677e832b7b
Younes Maleki <edmond.d1995@gmail.com>
2023-04-17 15:56:40 +0430

    add pages app to project and add home page and about us page view

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import generic
+
+
+class HomePageView(generic.TemplateView):
+    template_name = 'home.html'
+
+
+class AboutUsPageView(generic.TemplateView):
+    template_name = 'aboutus.html'
+
+
+
