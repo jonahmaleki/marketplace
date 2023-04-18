ffba2b54636ec4c58f66b5cf5223c51629b994cf
Younes Maleki <edmond.d1995@gmail.com>
2023-04-18 19:44:24 +0430

    add TabularInLine to admin product  and delete comment template tags

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,20 @@ from django.contrib.admin import ModelAdmin
 
 from .models import Product, Comment
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['body', 'author', 'is_active', 'stars', 'recommend']
+    extra = 0
+
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
     list_display = ['title', 'price', 'active']
 
+    inlines = [
+        CommentInline
+    ]
 @admin.register(Comment)
 class CommentAdmin(ModelAdmin):
     list_display = ['body', 'author', 'is_active', 'stars', 'recommend', 'product']
+
--- products/templatetags/comments_tags.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.library()
-
-@register.filter
-def only_active_comment(comment):
-    return comment.filter(active=True)
