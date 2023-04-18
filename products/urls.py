b29bd721ffeaa3c6f987e1e4cb9f6cefd34dbd0a
Younes Maleki <edmond.d1995@gmail.com>
2023-04-18 15:03:55 +0430

    define product list view and config url and template

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-
+    path('product_list/', views.ProductListView.as_view(), name='product_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    # model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(active=True)
