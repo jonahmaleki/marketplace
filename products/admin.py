5e424f0970837a149e99397f5b18aff6deaf151d
Younes Maleki <edmond.d1995@gmail.com>
2023-04-18 17:14:41 +0430

    add Comment model to product app

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from .models import Product
+from .models import Product, Comment
 
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
-
     list_display = ['title', 'price', 'active']
 
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ['body', 'author', 'is_active', 'stars', 'recommend', 'product']
