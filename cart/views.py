3b375954fb87e6603e88a59ef7336341df8e790e
Younes Maleki <edmond.d1995@gmail.com>
2023-04-19 20:34:32 +0430

    add cart detail view and define add to cart view in cart/views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from products.models import Product
 
-# Create your views here.
+from .cart import Cart
+from .forms import AddToCartProductForm
+
+def cart_detail_view(request):
+    cart = Cart(request)
+
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,9 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+
     path('rosetta/', include('rosetta.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
