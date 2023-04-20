9d17ad7736c65f858fc82108bf640f3a761fc102
Younes Maleki <edmond.d1995@gmail.com>
2023-04-20 18:38:10 +0430

    add remove from cart view

--- cart/views.py
+++ cart/views.py
@@ -6,6 +6,7 @@ from products.models import Product
 from .cart import Cart
 from .forms import AddToCartProductForm
 
+
 def cart_detail_view(request):
     cart = Cart(request)
 
@@ -24,5 +25,15 @@ def add_to_cart_view(request, product_id):
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
         cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
+
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart.remove(product)
 
     return redirect('cart:cart_detail')
--- config/settings.py
+++ config/settings.py
@@ -110,12 +110,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'asdf1997',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
