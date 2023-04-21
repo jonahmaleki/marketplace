698d5913d98716c05c2fc24197ae79793a9e12b9
Younes Maleki <edmond.d1995@gmail.com>
2023-04-21 22:44:13 +0430

    add context processor for get cart data in all templates

--- config/settings.py
+++ config/settings.py
@@ -81,6 +81,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                # local context proccesors
+                'cart.context_processors.cart',
             ],
         },
     },
