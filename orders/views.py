b80c6634706512e0473ce4b43aa9ce5b91b6f8ea
Younes Maleki <edmond.d1995@gmail.com>
2023-04-24 20:33:25 +0430

    get product from cart and send to order item model

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils.translation import gettext as _
+
 from .forms import OrderForm
+from cart.cart import Cart
+from .models import OrderItem
+
 
 @login_required
 def order_create_view(request):
     order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('your cart is empty, please select some product'))
+        return redirect('product_list')
 
     if request.method == 'POST':
         order_form = OrderForm(request.POST, )
@@ -14,6 +23,22 @@ def order_create_view(request):
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+            cart.clear()
+
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+
+            messages.success(request, _('your order has successfully placed'))
 
     return render(request, 'orders/order_create.html', context={
         'form': order_form,
